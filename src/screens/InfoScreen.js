f460e6c8019581a7df474e6cea4740310d78d390
Julius Rajala <julius.rajala@futurice.com>
2017-11-18 20:45:11 +0200

    Build info-screen.

--- src/screens/InfoScreen.js
+++ src/screens/InfoScreen.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { List } from 'immutable';
 import { connect } from 'react-redux';
+import logo from '../images/logo.png';
+import sponsor from '../images/sponsor.png';
+import './InfoScreen.css';
+
+const wordCloud = List.of(
+  '#jouluristeily',
+  '#jr17', '#ankkuriappro',
+  '#after_lecture', '#yoloristeily17',
+  '#blessed', '#avoint_lähdekoodii',
+);
 
 const Screen = (props) => {
   // const { ... } = props;
   return (
-    <div className="ScreenContainer">
-      <h2 className="ScreenContainer-title">Lisätietoja</h2>
+    <div className="ScreenContainer InfoScreen">
+      <img className="SplashScreen-logo" src={logo} />
+      <h2 className="SplashScreen-text">Risteilyllä mukana:</h2>
+      <img className="SplashScreen-logo SplashScreen-sponsor" src={sponsor} />
+      <div className="InfoScreen-some">
+        <h2 className="SplashScreen-text">Risteilyt somessa:</h2>
+        { wordCloud.map(hashtag => <bold>{hashtag}</bold>) }
+      </div>
     </div>
   );
 }
