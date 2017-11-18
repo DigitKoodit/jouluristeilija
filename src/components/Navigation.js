de35e79f83e7ff88c5681488523e7c8cf2cf5275
Julius Rajala <julius.rajala@futurice.com>
2017-11-18 22:23:43 +0200

    Added icons.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,13 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navigation.css';
 
+import boat from '../images/icon-boat.svg';
+import note from '../images/icon-note.svg';
+import info from '../images/icon-info.svg';
+import time from '../images/icon-time.svg';
+
+const routeIcons = {
+  'Map': boat,
+  'Cabins': note,
+  'Info': info,
+  'Schedule': time
+}
+
 const Navigation = ({ routes }) =>
   <ul className="Navigation">
     { routes.map((route, i) => {
       if (!route.symbol) return null;
       return (
         <li key={i} className="Navigation-Link">
-          <Link to={route.path}>{route.symbol}</Link>
+          <Link to={route.path}>
+            <img alt={route.symbol} className="Navigation-icon" src={routeIcons[route.symbol]} />
+          </Link>
         </li>
       ) }
     ) }
--- src/components/ZoomableMap.js
+++ src/components/ZoomableMap.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './ZoomableMap.css';
+import plusIcon from '../images/icon-plus.svg';
+import minusIcon from '../images/icon-minus.svg';
 
 const MIN_PADDING = 10;
 const MAX_PADDING = 60;
@@ -30,10 +32,10 @@ class ZoomableMap extends Component {
     return (
       <div className="ZoomableMap-container">
         { showZoomIn &&
-          <button onClick={() => this.zoomIn()} className="ZoomableMap-button">+</button>
+          <button onClick={() => this.zoomIn()} className="ZoomableMap-button"><img alt="Plus" src={plusIcon} /></button>
         }
         { showZoomOut &&
-          <button onClick={() => this.zoomOut()} className="ZoomableMap-button ZoomableMap-button--zoomOut">-</button>
+          <button onClick={() => this.zoomOut()} className="ZoomableMap-button ZoomableMap-button--zoomOut"><img alt="Minus" src={minusIcon} /></button>
         }
         <div style={{ padding: containerPadding }} className="ZoomableMap">
           <img className="ZoomableMap-image" src={mapSource} />
