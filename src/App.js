724b5b06a80bbd33f57c5db2ef2141ae76681f81
Ville Tainio <ville.tainio@futurice.com>
2017-11-18 19:50:19 +0200

    Store and fetch cabins from IndexedDB

--- src/App.js
+++ src/App.js
@@ -7,15 +7,29 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import AppRouter from './core/router';
 import routes from './core/routes';
+import cabinStorer from './middleware/cabinStorer';
+import { cabinsFetched, CABINS_DB_KEY } from './stores/cabins';
+import localforage from 'localforage';
 import './App.css';
 
 const App = () => {
   const store = compose(
-    applyMiddleware(thunk)
+    applyMiddleware(thunk),
+    applyMiddleware(cabinStorer)
   )(createStore)(
     rootReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // Enable redux devtools
   );
+
+  const fetchInitialSate = () => {
+    return Promise.resolve()
+      .then(() => localforage.getItem(CABINS_DB_KEY))
+      .then(value => {
+        store.dispatch(cabinsFetched(value));
+      })
+      .catch(err => console.error(err));
+  };
+  fetchInitialSate();
 
   return (
     <Provider store={store}>
